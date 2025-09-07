d8a86f71fa1e84cc1e7f7c93ef92e344c1ae84e0
HaeengIn <haeengin@gmail.com>
2025-09-07 20:21:42 +0900

    add 404 / subject route

--- app.py
+++ app.py
@@ -67,3 +67,10 @@ def index(request: Request):
 @app.get("/notice")
 def notice(request: Request):
     return templates.TemplateResponse("notice.html", {"request": request})
+
+@app.get("/{subject}")
+def cloud_subject(request: Request, subject: str):
+    valid_subjects = ["bio", "chem", "earth", "eng", "essay", "ethic", "gram", "jp", "kor", "math", "music", "pe", "phys", "stat"]
+    if subject not in valid_subjects:
+        return templates.TemplateResponse("404.html", {"request": request}, status_code=404)
+    return templates.TemplateResponse(f"cloud/{subject}.html", {"request": request})
