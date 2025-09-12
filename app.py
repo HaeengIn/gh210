48363b1d41fc8503fce733ed7a5a9032fb0013f4
HaeengIn <haeengin@gmail.com>
2025-09-12 05:00:33 +0000

    add write

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from supabase import create_client, Client
@@ -32,13 +32,45 @@ def notice(request: Request):
 def complain(request: Request):
     response = supabase.table("posts").select("*").execute()
     posts = response.data
+    for p in posts:
+        dt = datetime.fromisoformat(p['created_at'].replace("Z", "+00:00"))
+        p['created_at'] = dt.strftime("%Y-%m-%d")
+    for s in posts:
+        st = s['status']
+        if st == 0:
+            s['status'] = "대기"
+        elif st == 1:
+            s['status'] = "완료"
+        elif st == 2:
+            s['status'] = "공지"
     return templates.TemplateResponse("complain.html", {"request": request, "posts": posts})
 
-@app.get("/complain/{post_id}")
-def complainPost(request: Request, post_id: int):
-    response = supabase.table("posts").select("*").eq("id", post_id).single().execute()
+@app.get("/complain/{id}")
+def complainPost(request: Request, id: int):
+    response = supabase.table("posts").select("*").eq("id", id).single().execute()
     post = response.data
     return templates.TemplateResponse("posts.html", {"request": request, "post": post})
+
+@app.get("/write")
+async def write(request: Request):
+    return templates.TemplateResponse("write.html", {"request": request})
+
+@app.post("/write")
+async def writePost(request: Request, title: str = Form(...), content: str = Form(...)):
+    supabase.table("posts").insert({"title": title, "content": content, "status": 0}).execute()
+    response = supabase.table("posts").select("*").order("id", desc=True).execute()
+    posts = response.data
+    for p in posts:
+        dt = datetime.fromisoformat(p['created_at'].replace("Z", "+00:00"))
+        p['created_at'] = dt.strftime("%Y-%m-%d")
+        st = p['status']
+        if st == 0:
+            p['status'] = "대기"
+        elif st == 1:
+            p['status'] = "완료"
+        elif st == 2:
+            p['status'] = "공지"
+    return templates.TemplateResponse("complain.html", {"request": request, "posts": posts})
 
 @app.get("/{subject}")
 def cloud_subject(request: Request, subject: str):
