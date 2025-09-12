d8ddb74c6bf1248e7d8aaf890c94847b8089c8f8
HaeengIn <haeengin@gmail.com>
2025-09-12 15:34:38 +0900

    add newPost/Id

--- app.py
+++ app.py
@@ -59,6 +59,8 @@ async def write(request: Request):
 @app.post("/write")
 async def writePost(request: Request, title: str = Form(...), content: str = Form(...)):
     supabase.table("posts").insert({"title": title, "content": content, "status": 0}).execute()
+    newPost = response.data[0]
+    newId = newPost["id"]
     return RedirectResponse(url="/complain", status_code=303)
 
 @app.get("/{subject}")
